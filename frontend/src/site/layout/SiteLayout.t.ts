139a0ea23bef5f791b4b43e8567959ed06a9e089
Eduardo Kairalla <eduardokairalla123@gmail.com>
2026-09-21 11:26:45 -0300

    Add the site chrome layout

---
+++ frontend/src/site/layout/SiteLayout.t.ts
@@ -0,0 +1,9 @@
+/**
+ * Types for the SiteLayout.
+ */
+
+// --- GLOBALS ---
+export type SiteLayoutProps = {
+  /** The 404 drops the bar: there is no page to navigate within. */
+  nav?: boolean
+}
